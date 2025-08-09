f85b274fbc9c8d24383e135d3f7f5c22ceeabb3b
uthso297 <uthso119464@gmail.com>
2025-08-09 15:20:37 +0600

    created mode toggler

--- src/components/LayOut/Navbar.tsx
+++ src/components/LayOut/Navbar.tsx
@@ -11,6 +11,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { ModeToggle } from "./ModeToggler";
 
 // Navigation links array to be used in both desktop and mobile menus
 const navigationLinks = [
@@ -104,6 +105,7 @@ export default function Navbar() {
         </div>
         {/* Right side */}
         <div className="flex items-center gap-2">
+          <ModeToggle></ModeToggle>
           <Button asChild variant="ghost" size="sm" className="text-sm">
             <a href="#">Sign In</a>
           </Button>
