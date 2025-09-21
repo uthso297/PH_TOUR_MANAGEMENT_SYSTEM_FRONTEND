4b15a524828958dd0d21561b292f17f2dc193b5f
uthso297 <uthso119464@gmail.com>
2025-09-22 04:08:20 +0600

    integrated react hook form with shadcn

--- src/components/modules/Authentication/LoginForm.tsx
+++ src/components/modules/Authentication/LoginForm.tsx
@@ -25,91 +25,117 @@ export function LoginForm({
     try {
       const res = await login(data).unwrap();
       console.log(res);
-    } catch (err: unknown) {
-      if (typeof err === "object" && err !== null && "status" in err) {
-        const error = err as { status: number };
-        if (error.status === 401) {
-          toast.error("Your account is not verified");
-          navigate("/verify", { state: data.email });
+    // } catch (err) {
+    //   console.error(err);
+
+    //   if (err.status === 401) {
+    //     toast.error("Your account is not verified");
+    //     navigate("/verify", { state: data.email });
+    //   }
+    }
+
+     catch (err: unknown) {
+        if (typeof err === "object" && err !== null && "status" in err) {
+          console.error(err);
+          const error = err as { status: number };
+          if (error.status === 401) {
+            toast.error("Your account is not verified");
+            navigate("/verify", { state: data.email });
+          }
         }
       }
+
     }
-  };
 
-  return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
-        <p className="text-balance text-sm text-muted-foreground">
-          Enter your email below to login to your account
-        </p>
-      </div>
-      <div className="grid gap-6">
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="john@example.com"
-                      {...field}
-                      value={field.value || ""}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+    /*
+  
+    catch (err: unknown) {
+        if (typeof err === "object" && err !== null && "status" in err) {
+          const error = err as { status: number };
+          if (error.status === 401) {
+            toast.error("Your account is not verified");
+            navigate("/verify", { state: data.email });
+          }
+        }
+      }
+  
+  
+    */
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="********"
-                      {...field}
-                      value={field.value || ""}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-          </form>
-        </Form>
-
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
+    return (
+      <div className={cn("flex flex-col gap-6", className)} {...props}>
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Login to your account</h1>
+          <p className="text-balance text-sm text-muted-foreground">
+            Enter your email below to login to your account
+          </p>
         </div>
+        <div className="grid gap-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="john@example.com"
+                        {...field}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-        <Button
-          type="button"
-          variant="outline"
-          className="w-full cursor-pointer"
-        >
-          Login with Google
-        </Button>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="********"
+                        {...field}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </form>
+          </Form>
+
+          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            <span className="relative z-10 bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+
+          <Button
+            type="button"
+            variant="outline"
+            className="w-full cursor-pointer"
+          >
+            Login with Google
+          </Button>
+        </div>
+        <div className="text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link to="/register" replace className="underline underline-offset-4">
+            Register
+          </Link>
+        </div>
       </div>
-      <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <Link to="/register" replace className="underline underline-offset-4">
-          Register
-        </Link>
-      </div>
-    </div>
-  );
-}
+    );
+  }
